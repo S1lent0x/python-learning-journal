b5f40445978b08bd597400ed69561ad789d7b0c0
S1lent0x <ethanmeredith001@gmail.com>
2025-05-13 14:34:36 -0400

    Greeting exercise

---
+++ Week 1/Greeting Exercise.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = 2025 - int(input("What year were you born? "))
+hobby = input("What is a hobby of yours? ")
+print('Hello, ' + name + " it's nice to meet you!" + ' So far I know you are ' + str(age) + " years old" + 'and you enjoy ' + hobby + '.')
